c910ea1996c6a4a81f555d4ed300f8ee2924c68f
Ralph Gustave <rdgustave@gmail.com>
2022-01-25 00:40:30 -0800

    minor changes

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.get("/loco", (req, res) => {
 });
 
 app.get("/loca", (req, res) => {
-  res.send("This is locaaaaaaaaaaaaaaaa");
+  res.send(
+    `This chick is loca and her job title is ${process.env.PRINCIPAL_MODEL_TITLE1}`
+  );
 });
 
 const port = process.env.PORT || 8080;
